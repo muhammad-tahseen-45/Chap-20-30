bde25a30f9ac8238aa36d5eb087ca85e74443495
muhammad-tahseen-45 <144385363+muhammad-tahseen-45@users.noreply.github.com>
2023-09-26 13:55:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,181 @@
+// // chap 21
+
+// // 1
+// // var userinput = prompt();
+// // var userinput = userinput.toLocaleLowerCase();
+
+
+// // 2
+
+// var x = " HELLO WORLD "
+//  var x=x.toLocaleLowerCase();
+//  console.log(x);
+
+// //  3
+
+// var y = " hello world"
+// var y = y.toUpperCase();
+// console.log(y);
+
+
+
+// // 4
+
+// var   string = "HELLO WORLD";
+// var convert =  string.toLowerCase();
+// console.log(convert);
+
+// // 5
+
+// var   arry = ["HELLO","WORLD"]
+// var chanaging =  string.toLowerCase();
+// console.log(chanaging);
+
+
+// //6
+
+
+// var k = "hello world";
+// var k = k.toUpperCase();
+// alert(k);
+
+
+// // 7
+
+
+
+// var cityName = "karachi";
+// var firstchar = cityName.slice(0,1);
+// var firstchar = firstchar.toUpperCase();
+// var othercharct = cityName.slice(1);
+// var othercharct = othercharct.toLowerCase();
+// var combination = firstchar + othercharct;
+// console.log(combination);
+
+
+
+
+// chap 22-25
+
+// 1
+
+// var samweWord = "captian";
+// var samweWord = samweWord.slice(1,3);
+// console.log(samweWord);
+
+
+// // 2
+
+// var samweWord = "captian";
+// var samweWord = samweWord.length;
+// console.log(samweWord);
+
+// 3
+
+
+// var animal = "elephant";
+// var seg = animal.slice(2,6)
+// console.log(seg);
+
+// 4
+
+
+//  var samweWord = "captian";
+//  var samweWord = samweWord.length;
+// var samweWord = 9;
+
+// console.log(samweWord);
+
+
+// 5
+
+
+
+// var  string = "hello world is standard";
+// // var string = string.length;
+// var copy = string.slice(1,-3)
+// console.log(copy);
+
+
+// 6
+
+//  var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+
+// console.log(indx)
+
+
+
+//  7
+
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log(indx)
+
+
+// //  8
+
+
+// var tex = "go";
+//  var indx = tex.indexOf("g")
+//  console.log(indx);
+
+
+//  10
+
+// var string = "abcdef";
+// var string = string.charAt(2);
+// console.log(string)
+
+// // 11
+
+// var tex = "hello world";
+// var cha = tex.charAt(10);
+// console.log(cha);
+
+// 12
+
+// var str = "Hello World";
+// var x = str.charAt(str.length -1);
+
+// console.log(x);
+
+
+// 13
+
+// var tex = "helloworld";
+// var cha = tex.charAt(5);
+// console.log(cha);
+
+
+// // 14
+
+//  var string = " hello world"
+
+//  var string  = string.charAt(3)
+//  console.log(string);
+
+
+// //  15
+
+// var tex = "hello"
+// if(tex.charAt(3) === "l"){
+//     console.log(tex);
+// }
+
+
+
+// // 16
+
+
+
+
+
+
+
+
+
+    
+
+
+
